f7e054bdd8a24795a69531031be426213a01f418
boldak <boldak.andrey@gmail.com>
2017-11-17 09:35:22 +0200

    initial commit

---
+++ api/controllers/impl/index.js
@@ -0,0 +1,19 @@
+module.exports = [
+	require("./meta"),
+	require("./src"),
+	require("./lookup"),
+	require("./translate"),
+	require("./cache"),
+	// require("./definition"),
+	require("./dm/create"),
+	require("./dm/select"),
+	require("./dm/update"),
+	require("./dm/delete"),
+	require("./ddl/description"),
+	require("./ddl/create"),
+	require("./ddl/alter"),
+	require("./ddl/drop")
+	// ,require("./newmodel")
+
+
+]
